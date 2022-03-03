0367a0f6df0b437d986162ec59c128deff3e6e33
Jefferson Santos <jeffersonscr12@gmail.com>
2022-03-02 22:27:26 -0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,29 +1,74 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
+function userFactory (name, username) {
+  const newUser = { 
+    id: uuidv4(), 
+    name,
+    username,
+    todos: []
+  }
+  return newUser
+};
+
+function todoFactory (title , deadline) {
+  const newTodo ={ 
+    id: uuidv4(), 
+    title,
+    done: false, 
+    deadline:  new Date(deadline), 
+    created_at: new Date()
+  }
+  return newTodo
+};
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const findUser = users.find(user => user.username === username);
+
+  if(findUser){
+    next()
+  }else{
+    response.status(400).json({error: 'usuário não encontrado'})
+  }
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username }=  request.body;
+  if(!name || ! username){
+    response.status(400).json({error: 'name ou username nao digitado'})
+  }
+  const user = userFactory(name, username);
+  users.push(user);
+  response.status(200).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+  const findUser = users.find(user => user.username === username);
+  response.status(200).json(findUser.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { title , deadline }=  request.body;
+  const { username } = request.headers;
+  if(!title || ! deadline){
+    response.status(400).json({error: 'title ou deadline nao digitado'})
+  };
+  const todo = todoFactory (title , deadline);
+  const user = users.find(user => user.username === username);
+  user.todos.push(todo);
+  response.status(200).json({msg: 'todo armazenada'})
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
